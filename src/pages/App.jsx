64702bd41063873f2d743a889ccf5eb95234780f
leemit <ghahn97@gmail.com>
2023-01-18 15:46:02 +0900

    postpage init

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,17 +1,17 @@
 import { Route, Routes } from "react-router-dom";
-// import styled from "styled-components";
 import Incontents from "../components/content/incontents";
-import Home from "../components/inhome/home";
+import Home from "../components/home/home";
 import Nav from "../components/nav/nav";
+import Posthome from "../components/post/posthome";
 
 function App() {
   return (
     <>
       <Nav />
-      {/* <Categori /> */}
-        <Routes>
+        <Routes> 
           <Route path="/" element={<Home />} />
           <Route path="/content" element={<Incontents />} />
+          <Route path="/post" element={<Posthome />} />
         </Routes>
     </>
   );
