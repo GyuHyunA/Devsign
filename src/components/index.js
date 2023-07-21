a5557926abf297421e1bf70d448566b40ab63668
GyuHyunA <ghahn97@gmail.com>
2023-07-21 18:36:45 +0900

    리셋

--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-// User
-export { default as UNavi } from "./users/navi/navi";
-export { default as UHome } from "./users/home/index";
--- src/dummy/contentslist.js
+++
@@ -1,3 +0,0 @@
-export const uContList = [
-  {id:1, title: "제목1", contents:"콘텐츠1", }
-]
