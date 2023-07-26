c56fa57df760d5220e1d5050e92fe38f3c723030
GyuHyunA <ghahn97@gmail.com>
2023-07-26 16:36:31 +0900

    에러페이지 추가

--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -1,11 +1,13 @@
+import UError from "../components/error/uerror";
 import Uhome from "../components/uhome/uhome";
 
 export const routerlist = [
-    {
-        path: "/",
-        element: <Uhome/>,
-    },
-    {
-        path:"*",
-    },
-]
+  {
+    path: "/",
+    element: <Uhome />,
+  },
+  {
+    path: "*",
+    element: <UError />,
+  },
+];
